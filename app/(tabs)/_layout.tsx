bc4d412d446f4ef6c3da063ae5887ea09b3272da
ibrahimdeveloper <muhammadibrahimmughal35@gmail.com>
2025-06-06 21:09:18 +0500

    dev

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,6 +6,8 @@ import { SceneMap, TabView } from 'react-native-tab-view';
 import Home from '.'; // your Home component
 import Explore from './explore';
 import Profile from './profile';
+import Shorts from './Shorts';
+import uplaod from './uplaod';
 
 export default function SwipeTabsWithBottomBar() {
   const layout = useWindowDimensions();
@@ -13,14 +15,19 @@ export default function SwipeTabsWithBottomBar() {
 
   const [routes] = useState([
     { key: 'home', title: 'Home', icon: 'home-outline' },
-    { key: 'explore', title: 'Explore', icon: 'compass-outline' },
+   {key: 'shorts' ,title: 'Shorts',icon: "play-circle-outline" },
+    {key: 'uplaod', title: 'Uplaod', icon: 'cloud-upload'},
+     { key: 'explore', title: 'Explore', icon: 'compass-outline' },
     { key: 'profile', title: 'Profile', icon: 'person-circle-outline' },
   ]);
 
   const renderScene = SceneMap({
     home: Home,
     explore: Explore,
+    shorts: Shorts,
     profile: Profile,
+    uplaod: uplaod,
+    
   });
 
   return (
@@ -39,11 +46,13 @@ export default function SwipeTabsWithBottomBar() {
             style={styles.tabItem}
             onPress={() => setIndex(i)}
           >
-            <Ionicons
-              name={route.icon}
+                <Ionicons
+              name={ route.icon}
               size={24}
               color={i === index ? '#007AFF' : 'gray'}
             />
+
+ 
             <Text style={{ color: i === index ? '#007AFF' : 'gray' }}>
               {route.title}
             </Text>
---
+++ app/(tabs)/shorts.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function shorts() {
+  return (
+    <View>
+      <Text>Shorts</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
